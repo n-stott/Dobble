8081ca28d0b2576ec5a7b551d9c652573835c0ab
n-stott <aeastrolabe@gmail.com>
2020-11-16 19:29:18 +0100

    new implementaion

--- dobble_solver.cpp
+++ dobble_solver.cpp
@@ -2,18 +2,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
+#include <chrono>
 
-
-struct Problem {
-    int N;
-    int U;
-
-    Problem(int n) : N(n), U(N*(N-1)+1) { }
-};
+#define template_header int N, int U=N*(N-1)+1
 
 struct Logo {
     int id;
 
+    Logo() : id(-1) { }
     Logo(int id) : id(id) { }
 
     void next() {
@@ -24,93 +21,159 @@ struct Logo {
     bool operator!=(const Logo& other) const { return id != other.id; }
 };
 
-std::vector<int> work(100, 0);
+template<int U>
+struct MarkArray {
 
-struct Card {
-    std::vector<Logo> logos;
-
-    bool compatible(const Problem& p, const Card& other) const {
-        work.resize(p.U);
+    MarkArray() : work(), marker(1) {
         std::fill(work.begin(), work.end(), 0);
+    }
+
+    void clear() {
+        ++marker;
+        if(marker > 2.0e9) {
+            marker = 1;
+            std::fill(work.begin(), work.end(), 0);
+        }
+    }
+
+    int& operator[](int i) {
+        return work[i];
+    }
+
+    void mark(int i) {
+        work[i] = marker;
+    }
+
+    bool isMarked(int i) const {
+        return work[i] == marker;
+    }
+
+    std::array<int, U> work;
+    int marker;
+};
+
+template<template_header>
+struct Card {
+
+    Card() : logos(), active(), nz(0) {
+        std::fill(active.begin(), active.end(), 0);
+    }
+
+    std::array<Logo, N> logos;
+    std::array<int, U> active;
+    int nz;
+
+    static MarkArray<U> work;
+
+    bool compatibleWith(const Card& other) const {
+        check();
+        other.check();
         int collisions = 0;
-        for(Logo l : logos) work[l.id]++;
-        for(Logo l : other.logos) {
-            if(work[l.id]) collisions++;
-            work[l.id]++;
+        if(nz == 0) return true;
+        if(other.active[logos[nz-1].id] == 0) return true;
+        for(int i = 0; i < other.nz; ++i) {
+            int id = other.logos[i].id;
+            if(active[id]) collisions++;
         }
         return collisions <= 1;
     }
 
-    void push(const Problem& p) {
-        if(logos.empty()) {
-            logos.emplace_back(0);
-        } else {
-            logos.emplace_back(logos.back().id+1);
+    bool push() {
+        assert(nz < N);
+        int id = (nz == 0 ? 0 : logos[nz-1].id+1);
+        if(id >= U) return false;
+        logos[nz] = Logo(id);
+        if(id < U) active[id] = 1;
+        ++nz;
+        return true;
+    }
+    void push(Logo l) {
+        assert(nz < N);
+        logos[nz] = l;
+        assert(l.id >= 0 && l.id < U);
+        active[l.id] = 1;
+        ++nz;
+    }
+
+    bool valid() const {
+        return (nz == N) && std::all_of(logos.begin(), logos.begin()+nz, [](const Logo& l){ return l.id < U; });
+    }
+
+    bool next() {
+        assert(logos[nz-1].id >= 0 && logos[nz-1].id < U);
+        active[logos[nz-1].id] = 0;
+        logos[nz-1].next();
+        bool ok = (logos[nz-1].id < U);
+        if(ok) {
+            active[logos[nz-1].id] = 1;
         }
+        return ok;
     }
 
-    bool valid(const Problem& p) const {
-        return (logos.size() == p.N) && std::all_of(logos.begin(), logos.end(), [&p](const Logo& l){ return l.id < p.U; });
-    }
-
-    bool next(const Problem& p) {
-        logos.back().next();
-        return logos.back().id < p.U;
+    void check() const {
+        for(int i = 0; i < nz; ++i) {
+            assert(logos[i].id >= 0 && logos[i].id < U);
+            assert(active[logos[i].id]);
+        }
+        int acc = 0;
+        for(int ac : active) acc += ac;
+        assert(acc == nz);
     }
 
     std::string toString() const {
         std::string s;
-        for(Logo l : logos) s += std::to_string(l.id);
+        for(int i = 0; i< nz; ++i) s += std::to_string(logos[i].id) + " ";
         return s;
     }
 
 };
 
+template<int N, int U>
+MarkArray<U> Card<N, U>::work;
+
+template<template_header>
 struct Solution {
-    std::vector<Card> cards;
+    std::vector<Card<N,U>> cards;
     bool nil;
 
     Solution() : cards(), nil(false) {}
 
-    bool violates(const Problem& p) const {
+    bool violates() const {
         if(nil) return true;
-        if(cards.size() > p.U) return true;
-        for(int i = 0; i < cards.size(); ++i) {
-            for(int j = i+1; j < cards.size(); ++j) {
-                if(!cards[i].compatible(p, cards[j])) return true;
-            }
+        if(cards.size() > U) return true;
+        for(int i = 0; i < cards.size()-1; ++i) {
+            if(!cards.back().compatibleWith(cards[i])) return true;
         }
         return false;
     }
 
-    bool valid(const Problem& p) const {
+    bool valid() const {
         if(nil) return false;
-        if(cards.size() != p.U) return false;
-        if(!std::all_of(cards.begin(), cards.end(), [&p](const Card& c){ return c.valid(p); })) return false;
+        if(cards.size() != U) return false;
+        if(!std::all_of(cards.begin(), cards.end(), [](const Card<N,U>& c){ return c.valid(); })) return false;
         for(int i = 0; i < cards.size(); ++i) {
             for(int j = i+1; j < cards.size(); ++j) {
-                if(!cards[i].compatible(p, cards[j])) return false;
+                if(!cards[i].compatibleWith(cards[j])) return false;
             }
         }
         return true;
     }
 
-    void push(const Problem& p) {
+    void push() {
         if(cards.empty()) {
             cards.emplace_back();
-            cards.back().push(p);
         } else {
-            if(cards.back().logos.size() == p.N) {
+            if(cards.back().nz == N) {
                 cards.emplace_back();
-                cards.back().push(p);
-            } else {
-                cards.back().push(p);
             }
+        }
+        if(!cards.back().push()) {
+            nil = true;
         }
     }
 
-    void next(const Problem& p) {
-        if(!cards.back().next(p)) {
+    void next() {
+        if(!cards.back().next()) {
             nil = true;
         }
     }
@@ -123,91 +186,104 @@ struct Solution {
         std::string s;
         s += "nil?" + std::to_string(nil) + " ";
         s += "s" + std::to_string(cards.size()) + " ";
-        for(const Card& c : cards) s += c.toString() + " ";
+        for(const Card<N,U>& c : cards) s += c.toString() + " - ";
         return s;
     }
 };
 
-Solution root(const Problem& p) {
-    Solution r;
-    for(int i = 0; i < 1+2*(p.N-1); ++i) { r.cards.emplace_back(); }
-    r.cards[0].logos.resize(p.N, Logo{0});
-    // first card
-    for(int i = 0; i < p.N; ++i) {
-        r.cards[0].logos[i] = Logo{i};
+template<template_header>
+struct Solver {
+
+    Solution<N, U> root() {
+        Solution<N, U> r;
+        for(int i = 0; i < 1+2*(N-1); ++i) { r.cards.emplace_back(); }
+        // first card
+        for(int i = 0; i < N; ++i) {
+            r.cards[0].push(Logo{i});
+        }
+
+        // first "column" : cards with 0
+        for(int c = 0; c < N-1; ++c) {
+            r.cards[1+c].push(Logo{0});
+        }
+        int l = N;
+        for(int c = 0; c < N-1; ++c) {
+            for(int i = 1; i < N; ++i) {
+                r.cards[1+c].push(Logo{l});
+                ++l;
+            }
+        }
+
+        // first "column" : cards with 1
+        for(int c = 0; c < N-1; ++c) {
+            r.cards[N+c].push(Logo{1});
+        }
+        l = N;
+        for(int i = 1; i < N; ++i) {
+            for(int c = 0; c < N-1; ++c) {
+                r.cards[N+c].push(Logo{l});
+                ++l;
+            }
+        }
+
+        std::cout << "Root : " << r.toString() << "\n";
+        return r;
     }
 
-    // first "column" : cards with 0
-    for(int c = 0; c < p.N-1; ++c) {
-        r.cards[1+c].logos.push_back(Logo{0});
+    bool reject(const Solution<N, U>& sol) {
+        return sol.violates();
     }
-    int l = p.N;
-    for(int c = 0; c < p.N-1; ++c) {
-        for(int i = 1; i < p.N; ++i) {
-            r.cards[1+c].logos.push_back(Logo{l});
-            ++l;
+
+    bool accept(const Solution<N, U>& sol) {
+        return sol.valid();
+    }
+
+    Solution<N,U> first(const Solution<N, U>& candidate) {
+        Solution<N, U> s = candidate;
+        s.push();
+        return s;
+    }
+
+    void next(Solution<N, U>& sol) {
+        sol.next();
+    }
+
+    long long calls = 0;
+    std::chrono::system_clock::time_point begin;
+    std::chrono::system_clock::time_point current;
+
+    void backtrack(const Solution<N, U>& candidate) {
+        calls++;
+        if(calls % 10000000 == 0) {
+            current = std::chrono::high_resolution_clock::now();
+            auto elapsed = current - begin;
+            std::cout 
+                << (calls / 1.0e6) / (elapsed.count() / 1.0e9) << " Mcalls/s"
+                //<< elapsed.count() << " t"
+                //<< candidate.toString() 
+                << std::endl;    
+        }
+        if(reject(candidate)) return;
+        if(accept(candidate)) {
+            std::cout << "Solution found" << std::endl;
+            std::cout << candidate.toString() << std::endl;
+            std::cout << "Total calls : " << calls << "\n";
+            std::exit(0);
+        }
+        Solution<N, U> s = first(candidate);
+        while(!s.isNil()) {
+            backtrack(s);
+            next(s);
         }
     }
 
-    // first "column" : cards with 1
-    for(int c = 0; c < p.N-1; ++c) {
-        r.cards[p.N+c].logos.push_back(Logo{1});
+    Solver() {
+        begin = std::chrono::high_resolution_clock::now();
     }
-    l = p.N;
-    for(int i = 1; i < p.N; ++i) {
-        for(int c = 0; c < p.N-1; ++c) {
-            r.cards[p.N+c].logos.push_back(Logo{l});
-            ++l;
-        }
-    }
-
-    std::cout << "Root : " << r.toString() << "\n";
-    return r;
-}
-
-bool reject(const Problem& p, const Solution& sol) {
-    return sol.violates(p);
-}
-
-bool accept(const Problem& p, const Solution& sol) {
-    return sol.valid(p);
-}
-
-Solution first(const Problem& p, const Solution& candidate) {
-    Solution s = candidate;
-    s.push(p);
-    return s;
-}
-
-void next(const Problem& p, Solution& sol) {
-    sol.next(p);
-}
-
-int calls = 0;
-
-void backtrack(const Problem& p, const Solution& candidate) {
-    calls++;
-    if(calls % 1000000 == 0) std::cout << calls << std::endl; 
-    if(reject(p, candidate)) return;
-    if(accept(p, candidate)) {
-        std::cout << "Solution found" << std::endl;
-        std::cout << candidate.toString() << std::endl;
-        std::exit(0);
-    }
-    Solution s = first(p, candidate);
-    while(!s.isNil()) {
-        backtrack(p, s);
-        next(p, s);
-    }
-}
+};
 
 int main(int argc, const char* argv[]) {
-    if(argc > 1) {
-        Problem p(std::atoi(argv[1]));
-        std::cout << "N=" << p.N << " U=" << p.U << std::endl;
-        backtrack(p, root(p));
-    } else {
-        std::cout << "Usage : exe N \n";
-        std::cout << " with N = nb of logos per card\n"; 
-    }
+    constexpr int N = 6;
+    Solver<N> s; 
+    s.backtrack(s.root());
 }
